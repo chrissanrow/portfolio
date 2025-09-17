24791ebb3aafa25da52de03921bb5e0f41ab5d3b
Chris Sanrow <58667107+chrissanrow@users.noreply.github.com>
2025-09-16 22:18:15 -0700

    Add youtube embed

--- js/projects.js
+++ js/projects.js
@@ -9,6 +9,10 @@ function openPopup(elem) {
 function closePopup(elem) {
     let id = elem.id;
     id = id.substring(4);
-    console.log("overlay-" + id);
-    document.getElementById("overlay-" + id).style.height = "0%";
+    const overlay = document.getElementById("overlay-" + id);
+    overlay.style.height = "0%";
+    const iframe = overlay.querySelector('iframe');
+    if (iframe) {
+        iframe.src = iframe.src;
+    }
 }
