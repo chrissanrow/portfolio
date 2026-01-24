9ad19192d910bd6428f5e2afd70744d8bb75f442
Chris Sanrow <chrissanrow.dev@gmail.com>
2026-01-24 12:20:12 -0800

    UX changes and video playback fix

--- js/projects.js
+++ js/projects.js
@@ -15,4 +15,24 @@ function closePopup(elem) {
     if (iframe) {
         iframe.src = iframe.src;
     }
+
+    // Stop any playing videos
+    const videos = overlay.getElementsByTagName('video');
+    for (let i = 0; i < videos.length; i++) {
+        videos[i].pause();
+        videos[i].currentTime = 0;
+    }
 }
+
+// User input listener
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") {
+        const overlays = document.getElementsByClassName("overlay");
+        for (let i = 0; i < overlays.length; i++) {
+            if (overlays[i].style.height === "100%") {
+                closePopup({id: "btn-" + overlays[i].id.substring(8)});
+            }
+        }
+    }
+});
